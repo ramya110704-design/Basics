d78501dda5cc3c21ed4aaa84883a282ac642bc75
ramya110704-design <ramya110704@gmail.com>
2026-01-20 13:38:05 +0530

    ContainJava.java file created

---
+++ ContainJava.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ContainJava {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        if(str.contains("Java")){
+            System.out.println("The String contains Java");
+        }
+        else{
+            System.out.println("The String does not contain Java");
+        }
+    }
+    
+}
