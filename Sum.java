a020ee81e9e7d7b46b0937e80612014d5e4185b8
ramya110704-design <ramya110704@gmail.com>
2026-01-20 13:52:35 +0530

    Sum.java file created

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the Second number: ");
+        int b = sc.nextInt();
+        System.out.println("Sum of two numbers: "+(a+b));
+    }
+    
+}
