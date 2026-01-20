d653b875dea643f60fa8f36424bb5827fa529d8c
ramya110704-design <ramya110704@gmail.com>
2026-01-20 14:49:22 +0530

    Average.java file created

---
+++ Average.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many numbers: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            System.out.println("Enter the number "+i+":");
+            sum += sc.nextInt();
+        }
+        double avg = (double)sum/n;
+        System.out.println("Average:"+ avg);
+    }
+}
