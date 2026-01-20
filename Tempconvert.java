8b45fa9db7699ec479babe078daeb8d983b23145
ramya110704-design <ramya110704@gmail.com>
2026-01-20 12:35:34 +0530

    Tempconvert.java file created

---
+++ Tempconvert.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Tempconvert {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter temperature in Celsius: ");
+        double c = sc.nextDouble();
+        double f =(c* 9/5)+32;
+        System.out.println("Fahrenheit: "+f);
+
+    }
+    
+}
